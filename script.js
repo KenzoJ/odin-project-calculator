fcff70ef43b95e8f8e91bfef241e55069c922f64
Kenzo_Johnson <148596766+KenzoJ@users.noreply.github.com>
2024-05-04 18:12:09 -0400

    implement checks and measures

--- script.js
+++ script.js
@@ -20,15 +20,30 @@ function operate(operator, num1, num2) {
 function clearTerminal() {
     bottomScreen.textContent = ""
     topScreen.textContent = ""
-    
+    operators.forEach(operator => {
+        operator.classList?.remove("clicked")
+    })
+    firstNum = 0
+    secondNum = 0
 }
 
+function checkNum(number) {
+    if (parseInt(number, 10) > 0) {
+        return number
+    } else {
+        console.log("success")
+        clearTerminal()
+    } 
+}
 
 function main() {
+    equals.disabled = true
     //allow screen first num
     buttons.forEach(button => {
         button.addEventListener("click", event => {
-            bottomScreen.textContent = bottomScreen.textContent + event.currentTarget.innerText
+            if (bottomScreen.textContent.length < 12) {
+                bottomScreen.textContent = bottomScreen.textContent + event.currentTarget.innerText
+            }
         })
     })
 
@@ -37,18 +52,28 @@ function main() {
     //clear current numbers
     operators.forEach(operator => {
         operator.addEventListener("click", event => {
+            operator.classList.add("clicked")
            let currentOperator = event.currentTarget.innerText
            let firstNum = parseInt(bottomScreen.textContent, 10)
-           topScreen.textContent = firstNum + currentOperator
+           topScreen.textContent = firstNum
            bottomScreen.textContent = ""
+           equals.disabled = false
            equals.addEventListener("click", () => {
-            let secondNum = parseInt(bottomScreen.textContent, 10)
-            bottomScreen.textContent = operate(currentOperator, firstNum, secondNum)
-            topScreen.innerHTML = `&nbsp`
-            firstNum = 0
-            secondNum = 0
-        })
-    })})
+                operator.classList.remove("clicked")
+                let checkedFirstNum = checkNum(bottomScreen.textContent)
+                let secondNum = parseInt(checkedFirstNum, 10)
+                let answer = operate(currentOperator, firstNum, secondNum)
+                console.log("answer: " + answer)
+                if (answer === !NaN || answer == !undefined || answer) {
+                    bottomScreen.textContent = answer.toString().slice(0, 12)
+                    topScreen.innerHTML = `&nbsp`
+                } else {
+                    topScreen.innerHTML = `&nbsp`
+                    bottomScreen.textContent = "Err"
+
+                }
+            
+    }) })})
     
     //puts first number and second number 
     //in correct place and returns final result
